b9f7457a9ccef47434a7f088d65a44deb453e6fd
nampdn <nampdn@protonmail.com>
2020-01-16 15:03:26 +0700

    feat(s): implement basic s function

--- readme.ts
+++ readme.ts
@@ -1,12 +1,26 @@
-export type UnpackPromise<T> = T extends Promise<Array<infer U>>
-  ? U[]
-  : T extends Promise<infer D>
-  ? D
-  : T;
+import { F } from "ts-toolbelt";
 
-type DeltaFn<ARG extends any[], R> = (
-  ...args: UnpackPromise<ARG>
-) => R | PromiseLike<R>;
+function createBaraFormula<Fn extends F.Function>(f: Fn) {
+  return <T>(payload: T): PromiseLike<ReturnType<typeof f>> => Promise.resolve(f(payload))
+}
 
-function s() {}
-export default s;
+export default function s<Fns extends F.Function[]>(
+  ...args: F.Piper<Fns, "async">
+): F.Piped<Fns, "async"> {
+  return async payload => {
+    let last = payload;
+    for (const f of args) {
+      last = await createBaraFormula(f)(last);
+    }
+    return await Promise.resolve(last);
+  };
+}
+
+(async () => {
+  const result = await s(
+    async (a1: number) => Promise.resolve(`${a1}`),
+    async (z) => `${z} + meo` !== '',
+    async (b) => b === false
+  )(42);
+  result;
+})();
